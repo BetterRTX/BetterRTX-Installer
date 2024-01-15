fc511eb09b6fa27ca6966fb3121840e30dbf6dab
Jason Gardner <im@jasongardner.co>
2024-01-15 14:25:27 -0600

    fix: Add default locale

--- v2/installer.ps1
+++ v2/installer.ps1
@@ -1,6 +1,28 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
+$T = Data {
+    # Default
+    ConvertFrom-StringData -StringData @'
+    package_name = BetterRTX
+    browse = Browse...
+    install = Install
+    success = Success
+    error = Error
+    error_invalid_file_type = Invalid file type. Please select a .mcpack file.
+    error_no_installations_selected = Please select at least one Minecraft installation.
+    error_copy_failed = Unable to copy to side-loaded Minecraft installation. Please install IObit Unlocker.
+    setup = Setup
+    download = Download
+    launchers = Launchers
+    launchers_description = Use a sideloaded Minecraft installation
+    help = Help
+    backup_restore = Backup/Restore
+    backup = Backup
+    restore = Restore
+'@
+}
+
 Import-LocalizedData -BindingVariable T
 
 $ioBit = Get-StartApps | Where-Object { $_.Name -eq "IObit Unlocker" }
