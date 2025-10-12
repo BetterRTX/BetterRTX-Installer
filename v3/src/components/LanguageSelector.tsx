39169044b828e40d953aed24fb9d5c54a918617c
OrzMiku <miku@ecy.pink>
2025-10-12 21:47:48 +0800

    feat(i18n): zh-CN

--- v3/src/components/LanguageSelector.tsx
+++ v3/src/components/LanguageSelector.tsx
@@ -20,13 +20,13 @@ function LanguageSelector({
         onChange={onLanguageChange}
       >
         <option value="en">English</option>
+        <option value="zh-CN">简体中文</option>
         {/* <option value="fr">Français</option>
         <option value="de">Deutsch</option>
         <option value="es">Español</option>
         <option value="it">Italiano</option>
         <option value="pt">Português</option>
-        <option value="ru">Русский</option>
-        <option value="zh">中文</option> */}
+        <option value="ru">Русский</option> */}
         <option value="cimode">{translatorDebugLabel}</option>
       </select>
       <Globe className="language-menu-icon" size={16} />
