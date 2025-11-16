6722f3dbc4af73efc7550776c608eb6681eb35f2
OI-liyifan202201 <liyifan202201@outlook.com>
2025-11-16 19:39:00 +0800

    Update i18n.ts

--- v3/src/i18n.ts
+++ v3/src/i18n.ts
@@ -8,7 +8,7 @@ i18n
   .use(LanguageDetector)
   .use(HttpApi)
   .init({
-    supportedLngs: ['en'],
+    supportedLngs: ['en','zh'],
     fallbackLng: 'en',
     detection: {
       order: ['path', 'cookie', 'htmlTag', 'localStorage', 'subdomain'],
